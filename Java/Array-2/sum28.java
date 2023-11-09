e8fed814857abe06d8e0e7b5e7590dc483872df5
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-09 15:28:12 -0500

    Create sum28.java

---
+++ Java/Array-2/sum28.java
@@ -0,0 +1,8 @@
+public boolean sum28(int[] nums) {
+  int count = 0;
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] == 2) count++;
+  }
+  if(count*2 == 8) return true;
+  return false;
+}
