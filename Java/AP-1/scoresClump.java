8d188fcd9fcea21ab65c67da10221574521997eb
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-10 13:50:53 -0500

    Update scoresClump.java

--- Java/AP-1/scoresClump.java
+++ Java/AP-1/scoresClump.java
@@ -1,3 +1,8 @@
+/*  Given an array of scores sorted in increasing order,
+    return true if the array contains 3 adjacent scores 
+    that differ from each other by at most 2, 
+    such as with {3, 4, 5} or {3, 5, 5}.
+*/
 public boolean scoresClump(int[] scores) {
   for(int i = 1; i < scores.length-1;i++) {
     if(scores[i+1]-scores[i-1] <=2) return true;
