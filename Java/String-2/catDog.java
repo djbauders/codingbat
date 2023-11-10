01376c845b818493b9bae4a3f9a045ea67fdfb9d
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-10 08:53:59 -0500

    Create catDog.java

---
+++ Java/String-2/catDog.java
@@ -0,0 +1,8 @@
+public boolean catDog(String str) {
+  int catCount = 0, dogCount = 0;
+  for(int i = 0; i < str.length()-2;i++) {
+    if(str.substring(i,i+3).equals("cat")) catCount++;
+    if(str.substring(i,i+3).equals("dog")) dogCount++;
+  }
+  return catCount == dogCount;
+}
