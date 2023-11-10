b589fa794f2a37c827e45d9d94e8aef563483bdb
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-10 13:48:26 -0500

    Update scoresIncreasing.java

--- Java/AP-1/scoresIncreasing.java
+++ Java/AP-1/scoresIncreasing.java
@@ -1,3 +1,7 @@
+/*  Given an array of scores, return true if each score
+    is equal or greater than the one before.
+    The array will be length 2 or more.
+*/
 public boolean scoresIncreasing(int[] scores) {
   boolean flag = false;
   for(int i = 0; i < scores.length-1; i++) {
