c11a2eeeafe45472eaba95fca414b8e023baec4d
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-10 08:12:58 -0500

    Create matchUp.java

---
+++ Java/AP-1/matchUp.java
@@ -0,0 +1,7 @@
+public int matchUp(String[] a, String[] b) {
+  int count = 0;
+  for(int i = 0; i < a.length; i++) {
+    if(a[i] != "" && b[i] != "" && (a[i].substring(0,1).equals(b[i].substring(0,1)))) count++;
+  }
+  return count;
+}
