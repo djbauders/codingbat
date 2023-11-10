40e74300eb52612ecfd07475a3db84df4eeb3523
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-10 13:55:17 -0500

    Update wordsFront.java

--- Java/AP-1/wordsFront.java
+++ Java/AP-1/wordsFront.java
@@ -1,3 +1,6 @@
+/*  Given an array of strings, return a new array containing the 
+    first N strings. N will be in the range 1..length.
+*/
 public String[] wordsFront(String[] words, int n) {
   String[] res = new String[n];
   for(int i = 0; i < n; i++) {
