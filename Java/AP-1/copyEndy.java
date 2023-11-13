890196027251328481ee876e123e1e89bb8dfae3
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-13 10:34:55 -0500

    Update copyEndy.java

--- Java/AP-1/copyEndy.java
+++ Java/AP-1/copyEndy.java
@@ -1,3 +1,8 @@
+/*  We'll say that a positive int n is "endy" if it is 
+  in the range 0..10 or 90..100 (inclusive). Given an array
+  of positive ints, return a new array of length "count" 
+  containing the first endy numbers from the original array. Decompose out a separate isEndy(int n) method to test if a number is endy. The original array will contain at least "count" endy numbers.
+*/
 public int[] copyEndy(int[] nums, int count) {
   int[] res = new int[count];
   for(int i = 0, iter = 0; i < nums.length; i++) {
