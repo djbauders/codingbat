5a9d7b6a41cd95383896f3c5a27e371dd8b00029
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-13 10:30:14 -0500

    Update wordsWithoutList.java

--- Java/AP-1/wordsWithoutList.java
+++ Java/AP-1/wordsWithoutList.java
@@ -1,3 +1,8 @@
+/*  Given an array of strings, return a new List 
+    (e.g. an ArrayList) where all the strings of the given 
+    length are omitted. See wordsWithout() below which is 
+    more difficult because it uses arrays.
+*/
 public List wordsWithoutList(String[] words, int len) {
   ArrayList<String> lst = new ArrayList<String>();
   for(int i = 0; i < words.length; i++) {
