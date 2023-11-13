fb72d5141dc74078fa27b79926f45c454cdfce9c
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-13 10:34:06 -0500

    Update copyEvens.java

--- Java/AP-1/copyEvens.java
+++ Java/AP-1/copyEvens.java
@@ -1,6 +1,10 @@
+/*  Given an array of positive ints, return a new array
+    of length "count" containing the first even numbers
+    from the original array. The original array will 
+    contain at least "count" even numbers.
+*/
 public int[] copyEvens(int[] nums, int count) {
   int[] res = new int[count];
-  //int iter = 0;
   for(int i = 0,iter = 0; i < nums.length; i++) {
     if(nums[i]%2 == 0) {
       res[iter] = nums[i];
