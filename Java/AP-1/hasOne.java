5ddf99a89f0f2f626fd3dc06bd37785323ace3cc
djbauders <80076422+djbauders@users.noreply.github.com>
2023-11-13 10:31:00 -0500

    Update hasOne.java

--- Java/AP-1/hasOne.java
+++ Java/AP-1/hasOne.java
@@ -1,3 +1,8 @@
+/*  Given a positive int n, return true if it contains
+    a 1 digit. Note: use % to get the rightmost digit, and
+    / to discard the rightmost digit.
+*/
+
 public boolean hasOne(int n) {
   int mult = 10;
   if(n%10 == 1) return true;
